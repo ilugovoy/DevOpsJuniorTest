db553f3c385dabcceab79feff5937b3521b446ac
AEON <126128463+ilugovoy@users.noreply.github.com>
2025-06-08 19:03:27 +0300

    Delete roles directory

--- roles/docker_compose/files/init.sql
+++
@@ -1,6 +0,0 @@
-USE database; -- Попытка выбрать базу данных. Если её нет, возникнет ошибка, но следующая инструкция это обработает.
-CREATE TABLE IF NOT EXISTS app_table ( -- IF NOT EXISTS предотвратит ошибку, если таблица уже существует.
-    id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
-    status TEXT NOT NULL
-);
